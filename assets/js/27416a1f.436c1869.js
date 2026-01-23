1ba9affa854e945eba404e2a71ff87fefb821b78
Bodev13 <bo.sadriddin@gmail.com>
2026-01-23 14:50:51 +0100

    Deploy website - based on 855a349e6191fd96797a30dd43b7dd6d1167cfb5

---
+++ assets/js/27416a1f.436c1869.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdso_dev_blog=globalThis.webpackChunkdso_dev_blog||[]).push([[9013],{7552(o){o.exports=JSON.parse('{"metadata":{"permalink":"/devsecops-blog/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
